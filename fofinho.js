d455f7d437437707bf28887da9e182ebf764babe
brunnoli <59262172+brunnoli@users.noreply.github.com>
2024-01-21 15:19:30 -0300

    Add files via upload

---
+++ fofinho.js
@@ -0,0 +1,8 @@
+function togglemode() {
+    const html = document.documentElement    
+    if(html.classList.contains('branco')){
+    html.classList.remove('branco')
+    }else{html.classList.add('branco')        
+    }
+}
+    
